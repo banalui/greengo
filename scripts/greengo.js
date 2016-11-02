3575718d6ac2884eb46534a1e56112b94024358d
Behdad <behdad.analui@gmail.com>
2016-11-02 00:14:00 -0700

    bring jquery files locally and try to open a popup div

--- scripts/greengo.js
+++ scripts/greengo.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$("input").blur(function(){
+	/*$("input").blur(function(){
 
 		if($(this).val()){
 			$(this).addClass("used");
@@ -9,6 +9,17 @@ $(document).ready(function(){
 			$(this).removeClass("used");
 		}
 	});
-	$('.dropdown-toggle').dropdown()
+	$('.dropdown-toggle').dropdown()*/
 });
+
+function myFunction(){
+	console.log("Clicked Search");
+	$(".upperpartheading4 label").append('<div class="searchwindowdiv">SearchWindow</div>');
+	$(".searchwindowdiv").height(150);
+	$(".searchwindowdiv").width(200);
+	$(".searchwindowdiv").css("background-color", "orange");
+	/*$(".searchwindowdiv").css("position", "absolute");
+	$(".searchwindowdiv").css("top", "100px");
+	$(".searchwindowdiv").css("right", "350px");*/
+}
 
